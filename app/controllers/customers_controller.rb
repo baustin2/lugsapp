ead8ee009f44df21250e0df7233c2066d185e5a1
Tyler <astoundproduction@gmail.com>
2019-04-02 16:36:25 -0400

    customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -64,7 +64,7 @@ class CustomersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_customer
-      @customer = Customer.find(params[:id])
+      @customer = Customer.find(params[:customerNumber])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
