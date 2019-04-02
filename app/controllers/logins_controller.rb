63bd55827f39e52fb8a61797732a4568f3d6e07b
Tyler <astoundproduction@gmail.com>
2019-04-02 17:04:42 -0400

    tester

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -4,7 +4,7 @@ class LoginsController < ApplicationController
   def create
     customer = Customer.find_by(email: params[:logins][:email].downcase)
     session[:customer_customerNumber] = customer.customerNumber
-    if customer && customer.authenticate(params[:logins][:password])
+    if customer && customer.authenticate(params[:password])
       flash.notice = "You are now logged in."
       redirect_to customer
     else
