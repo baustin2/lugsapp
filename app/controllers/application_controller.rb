5e4e161270cd72f1af41888bfa1b631dbba97fac
Tyler <astoundproduction@gmail.com>
2019-04-03 08:10:55 -0400

    T

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
 helper_method :current_user, :logged_in?, :current_order
 def current_user
   @current_user ||= Customer.find(session[:customer_customerNumber]) if session[:customer_customerNumber]
