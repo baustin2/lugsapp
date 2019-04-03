db9b7dfd22fe45b0457d6d34e9ab322dc88f9177
Tyler <astoundproduction@gmail.com>
2019-04-03 16:07:23 -0400

    downcase

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -2,7 +2,7 @@ class LoginsController < ApplicationController
   def new
   end
   def create
-    customer = Customer.find_by(email: params[:logins][:email].downcase)
+    customer = Customer.find_by(email: params[:logins][:email])
     session[:customer_customerNumber] = customer.customerNumber
     if customer && customer.authenticate(params[:logins][:password])
       flash.notice = "You are now logged in."
